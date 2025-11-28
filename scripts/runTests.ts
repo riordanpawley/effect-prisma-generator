8a9b660f824780b2655a25c79755f50432e922dc
Mathieu Post <mathieupost@gmail.com>
2025-11-28 19:02:51 +0100

    chore: fix runTests.ts script

--- scripts/runTests.ts
+++ scripts/runTests.ts
@@ -2,20 +2,21 @@ import { Command, FileSystem } from "@effect/platform";
 import { NodeContext, NodeRuntime } from "@effect/platform-node";
 import { Console, Effect } from "effect";
 
-const run = (cmd: string, ...args: string[]) =>
+const run = (cwd: string, cmd: string, ...args: string[]) =>
   Effect.gen(function* () {
     yield* Console.log(cmd, ...args);
-    const exitCode = yield* Command.make(cmd, ...args).pipe(
-      Command.workingDirectory("tests"),
+    yield* Command.make(cmd, ...args).pipe(
+      Command.workingDirectory(cwd),
       Command.stdout("inherit"),
       Command.stderr("inherit"),
       Command.exitCode,
+      Effect.map(exitCode => {
+        if (exitCode !== 0) {
+          return Effect.fail(new Error(`Command failed with exit code ${exitCode}`));
+        }
+        return exitCode;
+      }),
     );
-    if (exitCode !== 0) {
-      yield* Effect.fail(
-        new Error(`Command failed with exit code ${exitCode}`),
-      );
-    }
   });
 
 const program = Effect.gen(function* () {
@@ -24,22 +25,22 @@ const program = Effect.gen(function* () {
 
   const distExists = yield* fs.exists("dist/");
   if (clean || !distExists) {
-    yield* run("npm", "run", "build");
+    yield* run(".", "npm", "run", "build");
   }
   if (clean) {
-    yield* run("tsc", "--noEmit");
+    yield* run(".", "tsc", "--noEmit");
   }
-  yield* run("prisma", "generate");
+  yield* run("./tests", "prisma", "generate");
   const dbExists = yield* fs.exists("prisma/dev.db");
   if (clean || !dbExists) {
-    yield* run("prisma", "db", "push");
+    yield* run("./tests", "prisma", "db", "push");
   }
-  yield* run("vitest", "run");
+  yield* run("./tests", "vitest", "run");
 }).pipe(
   Effect.ensuring(
     process.argv.includes("--keep-db")
       ? Effect.void
-      : Effect.ignore(run("rm", "-r", "prisma/dev.db")),
+      : Effect.ignore(run("./tests", "rm", "-f", "prisma/dev.db")),
   ),
 );
 
